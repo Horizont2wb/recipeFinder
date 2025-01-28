a3bf3ea95caa7095f0f49407719adc2f13017024
Will <160736269+Horizont2wb@users.noreply.github.com>
2025-01-28 17:56:06 +0100

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,7 @@
 """
 URL configuration for backend project.
 
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
